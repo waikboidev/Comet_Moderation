180364eb8c5d39285bc8c26c51ccb3fb1f01767e
waikboidev <waikefrfr@icloud.com>
2025-09-30 00:57:10 +1000

    Added

--- src/events/ready.js
+++ src/events/ready.js
@@ -8,6 +8,10 @@ module.exports = {
   execute(client) {
     logger.success(`${client.user.tag} is online!`);
 
+    // Heartbeat System for Status Page
+    axios.get(`https://uptime.betterstack.com/api/v1/heartbeat/${process.env.BETTERSTACK_HEARTBEAT}`)
+      .then(() => logger.info("Heartbeat was sent to Better Stack."))
+      .catch(err => logger.error("Heartbeat failed to send to Better Stack. Expect automatic incident report.", err));
     setInterval(() => {
       axios.get(`https://uptime.betterstack.com/api/v1/heartbeat/${process.env.BETTERSTACK_HEARTBEAT}`)
         .then(() => logger.info("Heartbeat was sent to Better Stack."))
