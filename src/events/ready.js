bd15444d4ebe2e2f24989c94ef51f4d2785ebe07
waikboidev <waikefrfr@icloud.com>
2025-09-30 00:54:07 +1000

    Added updates

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,17 @@
 const { Events } = require('discord.js');
 const { logger } = require('../utils/logger');
+const axios = require("axios");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     logger.success(`${client.user.tag} is online!`);
+
+    setInterval(() => {
+      axios.get(`https://uptime.betterstack.com/api/v1/heartbeat/${process.env.BETTERSTACK_HEARTBEAT}`)
+        .then(() => logger.info("Heartbeat was sent to Better Stack."))
+        .catch(err => logger.error("Heartbeat failed to send to Better Stack. Expect automatic incident report.", err));
+    }, 180 * 1000); // 3 min
   },
 };
